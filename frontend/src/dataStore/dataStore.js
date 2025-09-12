0133090ea11f4ab054e1a2a0b0ba7b1dce86155a
DPGFernando <dinethrag11@gmail.com>
2025-09-12 13:50:32 +0530

    update API_URL

--- frontend/src/dataStore/dataStore.js
+++ frontend/src/dataStore/dataStore.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "/api";
 
 export const useFetchCategories = () => {
   const [categories, setCategories] = useState([]);
